1b007ac3c6a0e324caad17d281c031e91eedb281
luongtrz <22120199@student.hcmus.edu.vn>
2026-01-09 22:03:25 +0700

    chore: remove backend/dist and backend/node_modules from git tracking

--- backend/dist/crypto/crypto.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { CryptoService, CoinData, HistoryPoint } from './crypto.service';
-export declare class CryptoController {
-    private readonly cryptoService;
-    constructor(cryptoService: CryptoService);
-    getTopCoins(): Promise<CoinData[]>;
-    getHistoricalData(symbol: string, limit?: string, aggregate?: string, type?: 'minute' | 'hour' | 'day'): Promise<HistoryPoint[]>;
-}
